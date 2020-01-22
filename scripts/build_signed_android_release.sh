f80795883661fa1c2a13fc1d4256686de57fe925
vsteinb <vsteinbruegg@uos.de>
2020-01-22 12:31:33 +0100

    re-add db in script build_signed_android_release.sh

--- scripts/build_signed_android_release.sh
+++ scripts/build_signed_android_release.sh
@@ -1,8 +1,15 @@
 # delete old version
-rm -rf ~/Schreibtisch/monsterdex/platforms/android/app/build/outputs/apk/release/*
+rm -rf ~/Schreibtisch/monsterdex/platforms/android/app/build/outputs
 
-# build new version
-ionic cordova build android --prod --release
+# build new version (1)
+#ionic cordova build android --prod --release split in two, because first one deletes www/ folder, therefore have to add it again
+ng run app:ionic-cordova-build:production --platform=android
+
+# copy db back,because www/ folder was deleted
+cp /home/vanessa/Downloads/res_monsterdex/monster.db /home/vanessa/schreibtisch/monsterdex/www/monster.db
+
+# build new version (2)
+cordova build android --release
 
 # goto directory of new version
 cd ~/Schreibtisch/monsterdex/platforms/android/app/build/outputs/apk/release
